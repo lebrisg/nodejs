ce0a563737c77ab87248ae33f73f4e8af0ee0473
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-12 11:46:23 +0100

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,19 +1,30 @@
-var http = require('http');
+var http = require('http'),
+      fs = require('fs');
+
+function serveStatFile(res, path, contentType, responseCode) {
+	if(!responseCode) responseCode = 200;
+	fs.readFile(__dirname = path, function(err,data) {
+		if(err) {
+			res.writeHead(500, { 'Content-Type': 'text/html' });
+			res.end('<html>500 - Internal Error</html>');
+		} else {
+			res.writeHead(500, { 'Content-Type': contentType });
+			res.end(data);
+		}
+	});
+}
 
 http.createServer(function(req,res){
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
         switch(path) {
 		case '':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html>Homepage</html>');
+			serveStaticFile(res, '/public/home.html', 'text/html');
 			break;
 		case '/about':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end('<html>About</html>');
+			serveStaticFile(res, '/public/about.html', 'text/html');
 			break;
 		default:
-			res.writeHead(404, { 'Content-Type': 'text/html' });
-			res.end('<html>Not Found</html>');
+			serveStaticFile(res, '/public/404.html', 'text/html',404);
 			break;
 	}
 }).listen(8080);
