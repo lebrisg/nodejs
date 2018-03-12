0b43413671a03a71217fd438b083d745e7a88660
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-12 11:51:20 +0100

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -5,10 +5,10 @@ function serveStatFile(res, path, contentType, responseCode) {
 	if(!responseCode) responseCode = 200;
 	fs.readFile(__dirname = path, function(err,data) {
 		if(err) {
-			res.writeHead(500, { 'Content-Type': 'text/html' });
-			res.end('<html>500 - Internal Error</html>');
+			res.writeHead(500, { 'Content-Type': 'text/plain' });
+			res.end('500 - Internal Error');
 		} else {
-			res.writeHead(500, { 'Content-Type': contentType });
+			res.writeHead(responseCode, { 'Content-Type': contentType });
 			res.end(data);
 		}
 	});
@@ -24,7 +24,7 @@ http.createServer(function(req,res){
 			serveStaticFile(res, '/public/about.html', 'text/html');
 			break;
 		default:
-			serveStaticFile(res, '/public/404.html', 'text/html',404);
+			serveStaticFile(res, '/public/404.html', 'text/html', 404);
 			break;
 	}
 }).listen(8080);
