93825f0b84bf7e4e2cf0353a7f14ba0f4efa8957
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-12 11:34:23 +0100

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -4,15 +4,15 @@ http.createServer(function(req,res){
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
         switch(path) {
 		case '':
-			res.writeHead(200, { 'Content-Type': 'test/html' });
+			res.writeHead(200, { 'Content-Type': 'text/html' });
 			res.end('<html>Homepage</html>');
 			break;
 		case '/about':
-			res.writeHead(200, { 'Content-Type': 'test/html' });
+			res.writeHead(200, { 'Content-Type': 'text/html' });
 			res.end('<html>About</html>');
 			break;
 		default:
-			res.writeHead(404, { 'Content-Type': 'test/html' });
+			res.writeHead(404, { 'Content-Type': 'text/html' });
 			res.end('<html>Not Found</html>');
 			break;
 	}
