8c6da80de1ace7c6b512720f89b745d0d3e02ea6
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-19 11:49:53 +0100

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,res){
 	res.writeHead(200, { 'Content-Type': 'text/html' });
-  	res.end('<html>Hello world!</html>');
+  	res.end('<html>Hello world2!</html>');
 }).listen(8080);
 
 console.log('Server started on localhost:8080; press Ctrl-C to terminate....');
