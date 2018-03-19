29c2186657b8104bdf95e391de2e16af1cf61a0e
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-19 09:52:49 +0100

    Update files

--- helloWorld.js
+++ helloWorld.js
@@ -1,33 +1,8 @@
-var http = require('http'),
-      fs = require('fs');
-
-function serveStaticFile(res, path, contentType, responseCode) {
-	if(!responseCode) responseCode = 200;
-	fs.readFile(__dirname = path, function(err,data) {
-		if(err) {
-			res.writeHead(500, { 'Content-Type': 'text/plain' });
-			res.end('500 - Internal Error');
-		} else {
-			res.writeHead(responseCode, { 'Content-Type': contentType });
-			res.end(data);
-		}
-	});
-}
+var http = require('http');
 
 http.createServer(function(req,res){
-	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-        switch(path) {
-		case '':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
- 		 	res.end('<html>Hello world2!</html>');
-			break;
-		case '/about':
-			serveStaticFile(res, '/public/about.html', 'text/html');
-			break;
-		default:
-			serveStaticFile(res, '/public/404.html', 'text/html', 404);
-			break;
-	}
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+  	res.end('<html>Hello world!</html>');
 }).listen(8080);
 
 console.log('Server started on localhost:8080; press Ctrl-C to terminate....');
